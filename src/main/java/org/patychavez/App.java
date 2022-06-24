4642ec63aa063f201bb3acee39582e57eee1d226
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2022-06-24 16:27:42 -0500

    String reverser example

--- src/main/java/org/patychavez/App.java
+++ src/main/java/org/patychavez/App.java
@@ -1,5 +1,7 @@
 package org.patychavez;
 
+import org.patychavez.stringreverser.StringReverser;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World again!" );
+        String output = StringReverser.main("MyString");
+        System.out.println(output);
     }
 }
---
+++ src/main/java/org/patychavez/stringreverser/StringReverser.java
@@ -0,0 +1,9 @@
+package org.patychavez.stringreverser;
+
+public class StringReverser {
+
+    public static String main(String input) {
+        return "example";
+    }
+
+}
