faec05c1b2d7040e5cf356e4f3445341d32f1685
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2022-06-27 18:14:10 -0500

    the scanner methods are implemented

--- src/main/java/org/patychavez/arraypractice/onedimensionalarray/OneDimensionalArray.java
+++ src/main/java/org/patychavez/arraypractice/onedimensionalarray/OneDimensionalArray.java
@@ -1,9 +1,21 @@
 package org.patychavez.arraypractice.onedimensionalarray;
 
+import java.util.Scanner;
+
 public class OneDimensionalArray {
 
+    Scanner input = new Scanner(System.in);
+
     public String [] requestData(){
-        String [] stringArray = {"nombre1", "nombre2","nombre3","nombre4", "nombre5"};
+        System.out.println("indicates the amount of data to enter:");
+        int dataNumber= input.nextInt();
+        input.nextLine();
+        String [] stringArray = new String[dataNumber];
+        for(int i=0; i<stringArray.length; i++){
+            System.out.println("enter the string number "+(i+1)+":");
+            String text= input.nextLine();
+            stringArray[i]=text;
+        }
         return stringArray;
     }
 }
